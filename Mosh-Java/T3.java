85cbd7ad16ef23949b078080e3023c3b19288f45
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-30 22:21:32 +0200

    Primitive variables

---
+++ Mosh-Java/T3.java
@@ -0,0 +1,25 @@
+
+class Main {
+
+	public static void main(String[] args){
+
+		// Primitive Type variables : 8 primitive type of variables.
+		// byte, short, int, long, float, double, char, boolean.
+
+		byte age = 30;
+		long viewsCount = 3_123_456_789L;
+		float price = 10.99F;
+		char letter = 'A';                   // single character with single ''
+		boolean isEligible = false;
+
+
+		System.out.println(viewsCount);
+		System.out.println(price);
+		System.out.println(letter);
+		System.out.println(isEligible);
+
+
+	}
+
+
+}
