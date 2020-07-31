6491f133cb7de246fa6deeccfd19d4bdc4e55b2b
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-31 18:17:25 +0200

    Arithmetic Operations

---
+++ Mosh-Java/T10.java
@@ -0,0 +1,38 @@
+
+// Constants and Arithematic expression 
+
+
+class Main{
+
+	public static void main(String[] args){
+
+		final float PI = 3.14F;          // Use final
+
+		int result = 10/3;
+
+		double result2 = (double)10 / (double) 3 ;
+
+		int x = 1;
+
+		int y1 = x++;        		// increment operator
+
+		int y2 = ++x;
+
+		x2 = x + 2;		        
+
+		System.out.println(result);
+
+		System.out.println(result2);
+
+		System.out.println(x);
+
+		System.out.println(y1);
+
+		System.out.println(y2);
+
+		System.out.println(x2);
+
+
+	}
+
+}
