2706e15510621800f02faae9f1a386e843510bac
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-31 10:10:13 +0200

    Reference type variable

---
+++ Mosh-Java/T4.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Date;
+
+class Main {
+
+	public static void main(String[] args){
+
+                // Reference type variable
+
+		byte age = 30;
+		Date now = new Date();
+		System.out.println(now);
+
+
+	}
+
+
+}
