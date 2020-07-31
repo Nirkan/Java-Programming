3e9eac77661acd04d6359851e4ab77e7b8f777ad
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-31 20:50:44 +0200

    Math Class

---
+++ Mosh-Java/T12.java
@@ -0,0 +1,38 @@
+
+
+class Main{
+
+	public static void main(String[] args){
+
+
+		int result = Math.round(1.1F);
+
+		int result2 = (int)Math.ceil(1.1f);
+
+		int result3 = (int)Math.floor(1.1F);
+
+		int result4 = Math.max(1,2);
+
+		double resutl5 = Math.round(Math.random() * 100);
+
+		int resutl6 = (int) Math.round(Math.random());
+
+
+		System.out.println(result);
+
+		System.out.println(result2);
+
+		System.out.println(result3);
+
+		System.out.println(result4);
+
+		System.out.println(resutl5);
+
+
+
+
+
+
+	}
+
+}
