961fa59e2ab55a294078ac09738c6af5935b1c29
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-07-31 14:10:23 +0200

    Escape sequences

---
+++ Mosh-Java/T7.java
@@ -0,0 +1,28 @@
+
+// Escape Sequences
+
+class Main {
+
+
+	public static void main(String[] args){
+
+		String message = "Hello \"Niraj\" ";
+
+		String message2 = "c:\\Windows\\...";
+
+		String message3 = "c:\tWindows\\...";
+
+		String message4 = "c:\nWindows\\...";
+
+		System.out.println(message);
+
+		System.out.println(message2);
+
+		System.out.println(message3);
+
+
+
+	}
+
+
+}
