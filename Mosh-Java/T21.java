d9a54fc5179bac61daec3f0e5058f624544fb418
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-01 18:21:57 +0200

    ForLoop

---
+++ Mosh-Java/T21.java
@@ -0,0 +1,27 @@
+
+// For loops
+
+
+class Main{
+
+	public static void main(String[] args){
+
+		for (int i = 0; i <= 5; i++){
+
+			System.out.println("Hello World" + i);
+
+			}
+
+
+		for (int  i = 5; i > 0; i--){
+
+			System.out.println("Hello World" + i);
+
+			}
+
+
+
+		}
+
+
+}
