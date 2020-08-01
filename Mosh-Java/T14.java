5ffa1ea91e994dfcf47dfb2caac4bab9de70a857
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-01 11:11:38 +0200

    Reading Input

---
+++ Mosh-Java/T14.java
@@ -0,0 +1,36 @@
+
+// Reading input
+
+
+
+import java.util.Scanner;
+
+
+class Main{
+
+	public static void main(String[] args){
+
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Name : ");
+
+		// byte age = scanner.nextByte();
+
+		// String name = scanner.nextLine();
+
+		String name2 = scanner.nextLine().trim();
+
+		// System.out.println("You are " + age);
+
+		// System.out.println("You are " + name);
+
+		System.out.println("You are " + name2);
+		
+
+
+
+		}
+
+
+}
