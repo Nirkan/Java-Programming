31c21541593a68609fad418647cf87dab5cb4f7f
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-01 19:32:01 +0200

    for each loop

---
+++ Mosh-Java/T23.java
@@ -0,0 +1,25 @@
+
+// for each loop
+
+
+class Main{
+
+	public static void main(String[] args){
+
+		String[] fruits = {"Apple", "Mango", "Orange"};
+
+
+		for (int i = furits.lenght; i > 0; i--)
+
+			System.out.println(fruits[i]);
+
+
+		for (String fruit : fruits)            // for each loop has limitations
+		
+			System.out.print(fruit);       // It cannot loop backward.
+
+
+
+	}
+
+}
