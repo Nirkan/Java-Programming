a730c9518c4e3a1984ec7d50173daab64595da1d
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-01 16:18:56 +0200

    Ifelse Statement

---
+++ Mosh-Java/T17.java
@@ -0,0 +1,28 @@
+
+// If Statements
+
+
+class Main{
+
+	public static void main(String[] args){
+
+		int temp = 32;
+		
+		if (temp > 30){
+			
+			System.out.println("It's a hot day");
+			System.out.println("Drink water");
+
+		} else if (temp > 20 && temp <= 30) {
+		
+			System.out.println("Beautiful Day");
+
+		} else {
+
+
+			System.out.println("Cold day");
+		}
+
+	}
+
+}
