f75311b10c2773f58c18c1c75fb9e38120da622e
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-01 17:21:28 +0200

    Switch statement

---
+++ Mosh-Java/T19.java
@@ -0,0 +1,38 @@
+
+// Switch statement
+
+
+class Main{
+
+	public static void main(String[] args){
+
+
+		String role = "admin";
+
+		switch (role){
+
+			case "admin":
+
+				System.out.println("You are a admin");
+				break;
+
+
+			case "moderator":
+
+				System.out.println("You are a moderator");
+				break;
+
+
+			default:
+
+				System.out.println("Your are a guest");
+				break;
+
+		}
+
+
+
+
+	}
+
+}
