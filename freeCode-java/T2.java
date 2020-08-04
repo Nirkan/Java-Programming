627b7662d74b699c59fb3fa540457552c0439b97
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-04 19:42:16 +0200

    Variables and DataTypes

---
+++ freeCode-java/T2.java
@@ -0,0 +1,46 @@
+
+class Main{
+
+	public static void main(String[] args){
+
+
+		// primitive : not changeable
+
+		int hello_world = 5;
+
+		double num2 = 5.0;
+		
+		boolean b = false;
+
+		char c= 't';          // only one character
+
+
+		// non-primitive 
+
+		String str = "hey there check 123";
+
+		
+		// Reassigning		
+
+		int niraj = hello_world;
+
+
+		System.out.println(hello_world);
+
+		System.out.println(num2);
+
+		System.out.println(b);
+
+		System.out.println(c);
+
+		System.out.println(str);
+
+
+		System.out.println(niraj);
+
+
+
+		}
+
+
+}
