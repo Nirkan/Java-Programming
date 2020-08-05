b40fd3090925f265431d51f3b0c281105c4a4773
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-05 15:58:19 +0200

    Basic operators

---
+++ freeCode-java/T3.java
@@ -0,0 +1,36 @@
+
+
+class Main{
+
+	public static void main(String[] args){
+
+		int x = 8;
+		
+		int y = 10;
+
+		int z = 55; 
+
+		int sum = x + y + z;
+
+		int mul = x * y * z;
+
+		double u = (double)z/(double)y;
+
+		double d = Math.pow(x,y);
+
+
+		System.out.println(x);
+		
+		System.out.println(sum);
+
+		System.out.println(mul);
+
+		System.out.println(u);
+
+		System.out.println(d);
+
+
+
+	}
+
+}
