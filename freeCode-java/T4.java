a5c3fc78cdb4d9acbd804349acacb2f262802d71
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-05 15:58:50 +0200

    Input and scanners

---
+++ freeCode-java/T4.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+class Main {
+
+
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+
+	String scanned = sc.next();
+
+	int scanned2 = sc.nextInt();
+
+	boolean scanned3 = sc.nextBoolean();
+
+
+
+	System.out.println(scanned);	
+
+	System.out.println(scanned2);
+
+	System.out.println(scanned3);
+
+
+
+	
+	}
+
+}
