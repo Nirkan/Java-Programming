9c74757fd0c9883e888c9a809a57b90f19b2569d
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-06 15:09:09 +0200

    Arrays

---
+++ freeCode-java/T8.java
@@ -0,0 +1,41 @@
+
+
+class Main{
+
+	public static void main(String[] args) {
+
+
+		String[]  newArr = new String[5];
+
+		newArr[0] = "hello";
+
+                newArr[1] = "hi";
+     
+                newArr[2] = "hillary";
+
+		newArr[3] = "bill";
+
+		newArr[4] = "joe";
+
+
+		int[] nums = {2,3,54,6,6};      // easier way of assigning an array
+
+		int y = nums [4];
+
+		String x = newArr[0];
+
+		double[] nums2 = {2.0,3.0};
+
+		double z = nums2[1];
+
+		System.out.println(x);
+
+		System.out.println(y);
+
+		System.out.println(z);
+
+
+		}
+
+
+}
