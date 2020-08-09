22bc280a5a9e64ef71f7c7283bb2faaf82f18710
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-09 15:49:04 +0200

    Invoking superclass constructor

---
+++ OOP-TutorialPoint/Age.java
@@ -0,0 +1,49 @@
+
+
+class Superclass {
+
+	int age;
+
+	
+	Superclass(int age) {
+
+		this.age = age;
+
+	}
+
+
+
+	public void getAge() {
+
+
+		System.out.println("The value of the variable named age in super class is: " + age);
+
+
+	}
+
+}
+
+
+
+
+class Subclass extends Superclass {
+
+
+	Subclass(int age) {
+
+		super(age);
+
+	}
+
+
+	public static void main(String args[]) {
+
+
+		Subclass s = new Subclass(24);
+
+		s.getAge();
+
+	}
+
+
+}
